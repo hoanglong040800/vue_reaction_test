a19b88a3efea4350859915d0852c679120433990
hoanglong040800 <hoanglong040800@gmail.com>
2021-05-17 17:28:56 +0700

    toggle box w/ delay

---
+++ src/components/Box.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="box" v-if="showBox" @click="boxClicked">
+    <h3>CLICK ME</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+
+  data() {
+    return {
+      showBox: false,
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.showBox = true;
+    }, this.delay);
+  },
+
+  methods: {
+    boxClicked() {
+      this.$emit("box-clicked");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  width: 400px;
+  height: 200px;
+  background: #6add6a;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <main>
+      <h1>Reaction Test</h1>
+
+      <button :disabled="isPlay" @click="start">Play</button>
+
+      <Box v-if="isPlay" :delay="delay" @box-clicked="boxClicked" />
+    </main>
+  </div>
+</template>
+
+<script>
+import Box from "../components/Box";
+import Result from "../components/Result";
+
+export default {
+  data() {
+    return {
+      isPlay: false,
+      delay: null,
+    };
+  },
+
+  components: { Box },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 3000;
+      this.isPlay = true;
+    },
+
+    boxClicked() {
+      this.isPlay = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  padding: 50px 0;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+button {
+  width: 100px;
+  height: 40px;
+  margin: 20px 0;
+  font-size: 1.2em;
+  background: #c4c4c4;
+  cursor: pointer;
+}
+</style>
